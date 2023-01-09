52d489a7553a757258a182244e4a5b026bca0dc7
Liplum <Li_plum@outlook.com>
2023-01-10 05:01:13 +0800

    added EasyLocalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/widgets.dart';
 
 import 'app.dart';
+import 'r.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    const MyApp().withEasyLocalization(),
+  );
+}
+
+extension _AppX on Widget {
+  Widget withEasyLocalization() {
+    return EasyLocalization(
+      supportedLocales: R.supportedLocales,
+      path: 'assets/l10n',
+      fallbackLocale: R.defaultLocale,
+      useFallbackTranslations: true,
+      child: this,
+    );
+  }
 }
---
+++ lib/r.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class R {
+  R._();
+
+  static const defaultLocale = Locale('en');
+  static const supportedLocales = [
+    defaultLocale,
+  ];
+}
