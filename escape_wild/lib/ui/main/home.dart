6d494e0726b05670697f13e2b3a62ecb0f21176b
Liplum <Li_plum@outlook.com>
2023-01-23 13:57:25 +0800

    build iOS

--- escape_wild/lib/ui/main/home.dart
+++ escape_wild/lib/ui/main/home.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:escape_wild/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rettulf/rettulf.dart';
 
 import 'game.dart';
 import 'mine.dart';
@@ -37,7 +38,7 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: buildBody(),
-      bottomNavigationBar: buildBottom(),
+      bottomNavigationBar: buildBottom().expanded(),
     );
   }
 
